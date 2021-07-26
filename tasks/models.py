92f987077dec445ee19f13ac747ebefeb73e6cb1
Abdusamad Abdusattorov <abdusamadabdusattorov1@gmail.com>
2021-07-26 13:47:22 +0500

    minor changes

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None, null=True)
     title = models.CharField('Task', max_length=50)
     completed = models.BooleanField('Completed', default=False)
     created = models.DateTimeField('Created', auto_now_add=True)
@@ -13,7 +14,7 @@ class Task(models.Model):
 
 
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
     task = models.ForeignKey(Task, on_delete=models.CASCADE, default=1)
     title = models.CharField('Note', max_length=50)
     description = models.TextField('Description', null=True, blank=True)
